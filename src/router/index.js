4ed129f7d0039a3a602e38e7dcf0b77bb8625184
marta1014 <lovemadenghui@126.com>
2020-02-02 13:02:19 +0800

    注册挂载登陆页 实现基础结构样式

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,16 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    // @是src的别名 vue cli项目特供
+    component: () => import('@/views/login')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="loginPage">
+    <van-nav-bar title="登陆" />
+    <van-cell-group>
+      <van-field required label="手机号" placeholder="请输入用户名" />
+    </van-cell-group>
+    <van-cell-group>
+      <van-field required label="验证码" placeholder="请输入用户名">
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <van-cell>
+        <van-button type="info">登陆</van-button>
+    </van-cell>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loginPage'
+}
+</script>
+
+<style lang="less" scoped>
+.loginPage{
+    .van-cell{
+        padding: 26px 16px;
+        .van-button{
+            width: 100%;
+    }
+    }
+
+}
+</style>

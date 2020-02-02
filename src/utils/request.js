3375aae688a64e78cac358cf6d8b1a7c280d197f
marta1014 <lovemadenghui@126.com>
2020-02-02 19:59:18 +0800

    请求拦截 统一处理token

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,7 @@
  */
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+import store from '@/store'
 
 // axios.create 方法：复制一个 axios
 const request = axios.create({
@@ -28,4 +29,31 @@ request.defaults.transformResponse = [function (data) {
     return {}
   }
 }]
+
+// Add a request interceptor
+request.interceptors.request.use(function (config) {
+  // 统一设置token
+  // Do something before request is sent
+  // 此处后端要求把token放到请求头中 使用指定名字 Authorization 格式
+  // config.headers 获取到本次的请求头对象 这是axios的固定API
+  const { user } = store.state
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+// Add a response interceptor
+request.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
 export default request

d09a21b895e35f00c40de0cb82ca49539bbd8a32
marta1014 <lovemadenghui@126.com>
2020-02-03 08:54:53 +0800

    样式调整 作用域样式的处理 => 深度作用操作符

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <!-- 导航栏 -->
-    <van-nav-bar title="首页" />
+    <van-nav-bar title="首页" fixed/>
     <!-- /导航栏 -->
 
     <!-- 频道列表 -->
@@ -42,7 +42,29 @@ export default {
     this.getUserChannels()
   }
 }
+
+// 如果想要在父组件中影响子组件样式：
+// 要么不要有作用域，那就是全局，影响任何组件
+
+// 如果有作用域
+// 默认只能影响到自子组件的根节点样式生效
+// 审查元素找到子组件根节点类名使用
+// 或者手动给子组件添加一个 class，它会自动添加到子组件根节点的 class 中
+// 如果需要影响的更深，则使用深度选择器：>>>、/deep/、::v-deep
+// >>> 在 less、sass、stylus 等 CSS 预处理器中会报错
+// 所以建议使用 /deep/ 或者 ::v-deep
 </script>
 
-<style>
+<style lang="less" scoped>
+.home{
+  padding-top: 90px;
+  padding-bottom: 50px;
+  /deep/ .van-tabs__wrap{
+    position: fixed;
+    top: 46px;
+    left: 0;
+    right: 0;
+    z-index: 1;
+  }
+}
 </style>

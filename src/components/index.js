9ec292dab9dc64b32e38336247a08fc71411325d
marta1014 <lovemadenghui@126.com>
2020-02-03 22:02:33 +0800

    搜索页面结构基础搭建 注册搜素结果列表组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import articleList from './home/article-list'
 import channelEdit from './home/channel-edit.vue'
-
+import searchList from './search/res-list'
 export default {
   install (Vue) {
     Vue.component('channel-edit', channelEdit)// 全局注册list组件
     Vue.component('article-list', articleList)
+    Vue.component('search-list', searchList)
   }
 }
---
+++ src/views/search/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="searchPage">
+    <!-- 搜索栏 -->
+    <form action="/">
+      <van-search
+        v-model="searchText"
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="$router.back()"
+      />
+    </form>
+    <!-- /搜索栏 -->
+
+    <!-- 联想建议 -->
+    <van-cell-group>
+      <van-cell title="单元格" icon="search" />
+      <van-cell title="单元格" icon="search" />
+    </van-cell-group>
+    <!-- /联想建议 -->
+
+    <!-- 历史记录 -->
+    <van-cell-group>
+      <van-cell title="历史记录">
+          <van-icon name="delete"></van-icon>
+          <span>全部删除</span>&nbsp;<span>完成</span>
+      </van-cell>
+      <van-cell title="单元格" >
+          <van-icon name="close"></van-icon>
+      </van-cell>
+    </van-cell-group>
+    <!-- /历史记录 -->
+
+    <!-- 搜索结果 -->
+    <search-list></search-list>
+    <!-- /搜索结果 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'searchPage',
+  data () {
+    return {
+      searchText: ''
+    }
+  },
+  methods: {
+    onSearch () {}
+  },
+  watch: {},
+  created () {}
+}
+</script>
+
+<style>
+</style>

5c3ec97127e892bdde81ce10ef108d955a550cdb
marta1014 <lovemadenghui@126.com>
2020-02-03 22:36:59 +0800

    视图（逻辑）显示状态 search固定

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="searchPage">
     <!-- 搜索栏 -->
-    <form action="/">
+    <form action="/" class="search">
       <van-search
         v-model="searchText"
         show-action
         placeholder="请输入搜索关键词"
         @search="onSearch"
+        @focus="resShow = false"
         @cancel="$router.back()"
       />
     </form>
     <!-- /搜索栏 -->
 
+     <!-- 搜索结果 -->
+    <search-list
+    v-if="resShow"></search-list>
+    <!-- /搜索结果 -->
+
     <!-- 联想建议 -->
-    <van-cell-group>
+    <van-cell-group v-else-if="searchText">
       <van-cell title="单元格" icon="search" />
       <van-cell title="单元格" icon="search" />
     </van-cell-group>
     <!-- /联想建议 -->
 
     <!-- 历史记录 -->
-    <van-cell-group>
+    <van-cell-group v-else>
       <van-cell title="历史记录">
           <van-icon name="delete"></van-icon>
           <span>全部删除</span>&nbsp;<span>完成</span>
@@ -30,10 +36,6 @@
       </van-cell>
     </van-cell-group>
     <!-- /历史记录 -->
-
-    <!-- 搜索结果 -->
-    <search-list></search-list>
-    <!-- /搜索结果 -->
   </div>
 </template>
 
@@ -42,16 +44,29 @@ export default {
   name: 'searchPage',
   data () {
     return {
-      searchText: ''
+      searchText: '',
+      resShow: false
     }
   },
   methods: {
-    onSearch () {}
+    onSearch () {
+      this.resShow = true
+    }
   },
   watch: {},
   created () {}
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.searchPage{
+    padding-top: 54px;
+    .search{
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        z-index: 1;
+    }
+}
 </style>

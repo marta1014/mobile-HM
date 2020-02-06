fa15ac33ad0d0061c9a8cf7f5948b69649bb1c6f
marta1014 <lovemadenghui@126.com>
2020-02-07 01:31:12 +0800

    获取评论回复并展示

--- src/components/article/comment-reply.vue
+++ src/components/article/comment-reply.vue
@@ -14,10 +14,21 @@
     <comment-item :comment="comment1"></comment-item>
     <!-- 需要拿到评论回复对象  -->
     <!-- /当前评论项 -->
+    <van-cell title="所有回复"></van-cell>
+    <!-- 回复列表 -->
+    <van-list
+    v-model="loading"
+    :finished="finished"
+    finished-text="没有更多了"
+    @load="onLoad">
+    <comment-item v-for="(item,index) of list" :key="index"
+    :comment="item"></comment-item>
+    </van-list>
   </div>
 </template>
 
 <script>
+import { getComments } from '@/api/article'
 export default {
   name: 'comment-reply',
   props: {
@@ -27,7 +38,35 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      list: [],
+      loading: false,
+      finished: false,
+      offset: null
+    }
+  },
+  methods: {
+    async onLoad () {
+      const { data } = await getComments({
+        type: 'c',
+        source: this.comment1.com_id.toString(),
+        offset: this.offset, // 获取下一页
+        limit: null
+      })
+
+      const { results } = data.data
+      this.list.push(...results)
+
+      // 加载状态结束
+      this.loading = false
+
+      // 数据全部加载完成
+      if (results.length) {
+        this.offset = data.data.last_id // 更新页码
+      } else {
+        this.finished = true
+      }
+    }
   }
 }
 </script>
